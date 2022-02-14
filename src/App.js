7432825e5dd31fa88a2f10a567b291c0e1296e0d
unknown <thalesalves758@gmail.com>
2022-02-14 17:12:04 -0300

    Adiciona a tela de redefinicao de senha e comenta a de login

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 
 import Login from './forms/Login';
+import RedefinirSenha from './forms/RedefinirSenha';
 
 function App() {
   return (
     <div className="App">
-      <Login></Login>
+      {/* <Login></Login> */}
+      <RedefinirSenha></RedefinirSenha>
     </div>
   );
 }
